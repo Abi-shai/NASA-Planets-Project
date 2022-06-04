ddd29e78fee96213b83d57cee364597143edd455
Abishai Yapélé <abishaiyp.dev@gmail.com>
2022-06-04 15:49:04 +0000

    Implementing testing and installing Jest dev-dependencie⬆️

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,14 @@
+describe('Test GET /launches', () => {
+    test('It should respond with a 200 success status code', () => {
+        const response = 200
+        expect(response).toBe(200)
+    })
+})
+
+describe('Test POST /launch', () => {
+    test('It should respond with a 200 success status code', () => {
+
+    })
+    test('It should catch missing required properties', () => {})
+    test('It should catch invalid date', () => {})
+})
