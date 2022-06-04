8ef1fa90599e0813b8d9ff39b5c70947b754139d
Abishai Yapélé <abishaiyp.dev@gmail.com>
2022-06-04 15:55:09 +0000

    Installing supertest dev-dependencie on the server folder⬆️

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,6 +1,9 @@
+const request = require('supertest')
+const app = require('../../app.js')
+
 describe('Test GET /launches', () => {
     test('It should respond with a 200 success status code', () => {
-        const response = 200
+        const response = request(app)
         expect(response).toBe(200)
     })
 })
